ef8ce3b58f1db9d09c363f0282ce38a4e1a47f81
Christopher Doctor <chris.doctor@tigerspike.com>
2024-06-17 01:32:53 +0800

    Update graphql schema to add additional lotttie object metadata

--- src/schema.ts
+++ src/schema.ts
@@ -1,30 +1,72 @@
 import {
   GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from "graphql";
 import mongoose from "mongoose";
 
+// Lottie file definition
+interface LottieFile {
+  filename: string;
+  contents: string;
+}
+
 // Mongoose item document
 interface ItemDocument extends mongoose.Document {
   id: string;
   description: string;
+  author: string;
+  tags: string[];
+  dateUploaded: string;
+  lottieFile: LottieFile;
 }
 
-const itemSchema = new mongoose.Schema({
-  id: String,
-  description: String,
+const LottieFileSchema: mongoose.Schema = new mongoose.Schema({
+  filename: { type: String, required: true },
+  contents: { type: String, required: true },
 });
 
-const Item = mongoose.model<ItemDocument>("Item", itemSchema);
+const ItemSchema: mongoose.Schema = new mongoose.Schema({
+  id: { type: String, required: true },
+  description: { type: String, required: true },
+  author: { type: String, required: true },
+  tags: { type: [String], required: true },
+  dateUploaded: { type: String, required: true },
+  lottieFile: { type: LottieFileSchema, required: true },
+});
 
-// The GraphQL Object type
+const Item = mongoose.model<ItemDocument>("Item", ItemSchema);
+
+// The GraphQL Object type for LottieFile
+const LottieFileType = new GraphQLObjectType({
+  name: "LottieFile",
+  fields: {
+    filename: { type: GraphQLString },
+    contents: { type: GraphQLString }, // Assuming contents are stored as JSON string
+  },
+});
+
+// The GraphQL Object type for the main item in db
 const ItemType = new GraphQLObjectType({
   name: "Item",
   fields: {
     id: { type: GraphQLID },
     description: { type: GraphQLString },
+    author: { type: GraphQLString },
+    tags: { type: new GraphQLList(GraphQLString) },
+    dateUploaded: { type: GraphQLString },
+    lottieFile: { type: LottieFileType },
+  },
+});
+
+const LottieFileInputType = new GraphQLInputObjectType({
+  name: "LottieFileInput",
+  fields: {
+    filename: { type: GraphQLString },
+    contents: { type: GraphQLString },
   },
 });
 
@@ -39,6 +81,19 @@ const Query = new GraphQLObjectType({
         return Item.findOne({ id: args.id });
       },
     },
+    searchItems: {
+      type: new GraphQLList(ItemType),
+      args: { keyword: { type: GraphQLString } },
+      resolve(_parent, args) {
+        const keywordRegex = new RegExp(args.keyword, "i");
+        return Item.find({
+          $or: [
+            { description: { $regex: keywordRegex } },
+            { tags: { $regex: keywordRegex } },
+          ],
+        });
+      },
+    },
   },
 });
 
@@ -51,11 +106,19 @@ const Mutation = new GraphQLObjectType({
       args: {
         id: { type: GraphQLString },
         description: { type: GraphQLString },
+        author: { type: GraphQLString },
+        tags: { type: new GraphQLList(GraphQLString) },
+        dateUploaded: { type: GraphQLString },
+        lottieFile: { type: LottieFileInputType },
       },
       resolve(_parent, args) {
         const item = new Item({
           id: args.id,
           description: args.description,
+          author: args.author,
+          tags: args.tags,
+          dateUploaded: args.dateUploaded,
+          lottieFile: args.lottieFile,
         });
         return item.save();
       },
